9464c55b44a41f610076d49e443e5306ac30bf05
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-12-08 14:40:27 +0530

    bumped submodule, commented code not needed for quickstart and bumped watsonx.ai

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -4,31 +4,16 @@
 
 locals {
 
-  prefix            = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}-" : "" : ""
-  service_endpoints = "public-and-private"
+  prefix = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}-" : "" : ""
 
   # KMS
   key_ring_name    = "${local.prefix}keyring"
   key_name         = "${local.prefix}key"
   wx_data_key_name = "${local.prefix}wxd-key"
 
-  # ICD Elastic Search
-  es_credentials = {
-    "elasticsearch_admin" : "Administrator",
-    "elasticsearch_operator" : "Operator",
-    "elasticsearch_viewer" : "Viewer",
-    "elasticsearch_editor" : "Editor",
-  }
-
-  # Watson
   watson_plan = {
-    "studio"      = "professional-v1",
-    "runtime"     = "v2-professional",
-    "discovery"   = "plus",
-    "assistant"   = "enterprise",
-    "governance"  = "essentials",
-    "data"        = "lakehouse-enterprise",
-    "orchestrate" = "essentials-agentic-mau"
+    "studio"  = "professional-v1",
+    "runtime" = "v2-professional"
   }
 
 }
@@ -92,7 +77,7 @@ data "ibm_iam_auth_token" "restapi" {}
 
 module "watsonx_ai" {
   source            = "terraform-ibm-modules/watsonx-ai/ibm"
-  version           = "2.10.2"
+  version           = "2.12.0"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
   resource_tags     = var.resource_tags
@@ -107,101 +92,6 @@ module "watsonx_ai" {
   enable_cos_kms_encryption = true
   cos_instance_crn          = module.cos.cos_instance_crn
   cos_kms_key_crn           = module.key_protect_all_inclusive.keys["${local.key_ring_name}.${local.key_name}"].crn
-}
-
-##############################################################################################################
-# Watson Discovery
-##############################################################################################################
-
-module "watson_discovery" {
-  source                = "terraform-ibm-modules/watsonx-discovery/ibm"
-  version               = "1.11.6"
-  region                = var.region
-  resource_group_id     = module.resource_group.resource_group_id
-  resource_tags         = var.resource_tags
-  plan                  = local.watson_plan["discovery"]
-  watson_discovery_name = "${local.prefix}discovery"
-  service_endpoints     = local.service_endpoints
-}
-
-##############################################################################################################
-# watsonx Assistant
-##############################################################################################################
-
-module "watsonx_assistant" {
-  source                 = "terraform-ibm-modules/watsonx-assistant/ibm"
-  version                = "1.5.7"
-  region                 = var.region
-  resource_group_id      = module.resource_group.resource_group_id
-  resource_tags          = var.resource_tags
-  plan                   = local.watson_plan["assistant"]
-  watsonx_assistant_name = "${local.prefix}assistant"
-  service_endpoints      = local.service_endpoints
-}
-
-##############################################################################################################
-# watsonx.governance
-##############################################################################################################
-
-module "watsonx_governance" {
-  source                  = "terraform-ibm-modules/watsonx-governance/ibm"
-  version                 = "1.11.6"
-  region                  = var.region
-  resource_group_id       = module.resource_group.resource_group_id
-  plan                    = local.watson_plan["governance"]
-  watsonx_governance_name = "${local.prefix}governance"
-  resource_tags           = var.resource_tags
-}
-
-##############################################################################################################
-# watsonx.data
-##############################################################################################################
-
-module "watsonx_data" {
-  source                        = "terraform-ibm-modules/watsonx-data/ibm"
-  version                       = "1.12.7"
-  region                        = var.region
-  resource_group_id             = module.resource_group.resource_group_id
-  watsonx_data_name             = "${local.prefix}data"
-  plan                          = local.watson_plan["data"]
-  use_case                      = "workloads"
-  resource_tags                 = var.resource_tags
-  enable_kms_encryption         = true
-  skip_iam_authorization_policy = false
-  watsonx_data_kms_key_crn      = module.key_protect_all_inclusive.keys["${local.key_ring_name}.${local.wx_data_key_name}"].crn
-}
-
-##############################################################################################################
-# watsonx Orchestrate
-##############################################################################################################
-
-module "watsonx_orchestrate" {
-  source                   = "terraform-ibm-modules/watsonx-orchestrate/ibm"
-  version                  = "1.11.6"
-  region                   = var.region
-  resource_group_id        = module.resource_group.resource_group_id
-  watsonx_orchestrate_name = "${local.prefix}orchestrate"
-  plan                     = local.watson_plan["orchestrate"]
-  resource_tags            = var.resource_tags
-}
-
-##############################################################################################################
-# Elastic search
-##############################################################################################################
-
-module "icd_elasticsearch" {
-  source                   = "terraform-ibm-modules/icd-elasticsearch/ibm"
-  version                  = "2.7.1"
-  resource_group_id        = module.resource_group.resource_group_id
-  name                     = "${local.prefix}data-store"
-  region                   = var.region
-  plan                     = "enterprise"
-  elasticsearch_version    = "9.1"
-  tags                     = var.resource_tags
-  service_endpoints        = "private"
-  member_host_flavor       = "multitenant"
-  deletion_protection      = false
-  service_credential_names = local.es_credentials
 }
 
 ##############################################################################################################
@@ -222,7 +112,7 @@ module "icr_endpoint" {
 }
 
 ##############################################################################################################
-# Code Engine - Project, Secret, Build, Application
+# Code Engine
 ##############################################################################################################
 
 locals {
@@ -309,17 +199,125 @@ module "code_engine" {
   }
 }
 
-##############################################################################
-# Code Engine Application
-##############################################################################
+# Deploy the sample app on code engine
 module "code_engine_app" {
   source            = "terraform-ibm-modules/code-engine/ibm//modules/app"
   version           = "4.7.0"
   project_id        = module.code_engine.project_id
   name              = local.ce_app_name
   image_reference   = module.code_engine.build["${local.prefix}ce-build"].output_image
-  image_secret      = module.code_engine.secret["${local.prefix}registry-secret"].name #module.code_engine_secret.name
+  image_secret      = module.code_engine.secret["${local.prefix}registry-secret"].name
   run_env_variables = local.env_vars
 }
 
-# #############################################################################################################
+
+# Uncomment the following code if additional resources are needed. 
+# Note: the Quickstart variation does not support these resources by default but they can be enabled for extended usage.
+
+# locals {
+
+#   service_endpoints = "public-and-private"
+
+#   # ICD Elastic Search
+#   es_credentials = {
+#     "elasticsearch_admin" : "Administrator",
+#     "elasticsearch_operator" : "Operator",
+#     "elasticsearch_viewer" : "Viewer",
+#     "elasticsearch_editor" : "Editor",
+#   }
+# }
+
+##############################################################################################################
+# Watson Discovery
+##############################################################################################################
+
+# module "watson_discovery" {
+#   source                = "terraform-ibm-modules/watsonx-discovery/ibm"
+#   version               = "1.11.6"
+#   region                = var.region
+#   resource_group_id     = module.resource_group.resource_group_id
+#   resource_tags         = var.resource_tags
+#   plan                  = "plus"
+#   watson_discovery_name = "${local.prefix}discovery"
+#   service_endpoints     = local.service_endpoints
+# }
+
+# ##############################################################################################################
+# # watsonx Assistant
+# ##############################################################################################################
+
+# module "watsonx_assistant" {
+#   source                 = "terraform-ibm-modules/watsonx-assistant/ibm"
+#   version                = "1.5.7"
+#   region                 = var.region
+#   resource_group_id      = module.resource_group.resource_group_id
+#   resource_tags          = var.resource_tags
+#   plan                   = "enterprise"
+#   watsonx_assistant_name = "${local.prefix}assistant"
+#   service_endpoints      = local.service_endpoints
+# }
+
+# ##############################################################################################################
+# # watsonx.governance
+# ##############################################################################################################
+
+# module "watsonx_governance" {
+#   source                  = "terraform-ibm-modules/watsonx-governance/ibm"
+#   version                 = "1.11.6"
+#   region                  = var.region
+#   resource_group_id       = module.resource_group.resource_group_id
+#   plan                    = "essentials"
+#   watsonx_governance_name = "${local.prefix}governance"
+#   resource_tags           = var.resource_tags
+# }
+
+# ##############################################################################################################
+# # watsonx.data
+# ##############################################################################################################
+
+# module "watsonx_data" {
+#   source                        = "terraform-ibm-modules/watsonx-data/ibm"
+#   version                       = "1.12.7"
+#   region                        = var.region
+#   resource_group_id             = module.resource_group.resource_group_id
+#   watsonx_data_name             = "${local.prefix}data"
+#   plan                          = "lakehouse-enterprise"
+#   use_case                      = "workloads"
+#   resource_tags                 = var.resource_tags
+#   enable_kms_encryption         = true
+#   skip_iam_authorization_policy = false
+#   watsonx_data_kms_key_crn      = module.key_protect_all_inclusive.keys["${local.key_ring_name}.${local.wx_data_key_name}"].crn
+# }
+
+# ##############################################################################################################
+# # watsonx Orchestrate
+# ##############################################################################################################
+
+# module "watsonx_orchestrate" {
+#   source                   = "terraform-ibm-modules/watsonx-orchestrate/ibm"
+#   version                  = "1.11.6"
+#   region                   = var.region
+#   resource_group_id        = module.resource_group.resource_group_id
+#   watsonx_orchestrate_name = "${local.prefix}orchestrate"
+#   plan                     = "essentials-agentic-mau"
+#   resource_tags            = var.resource_tags
+# }
+
+# ##############################################################################################################
+# # Elastic search
+# ##############################################################################################################
+
+# module "icd_elasticsearch" {
+#   source                   = "terraform-ibm-modules/icd-elasticsearch/ibm"
+#   version                  = "2.7.1"
+#   resource_group_id        = module.resource_group.resource_group_id
+#   name                     = "${local.prefix}data-store"
+#   region                   = var.region
+#   plan                     = "enterprise"
+#   elasticsearch_version    = "9.1"
+#   tags                     = var.resource_tags
+#   service_endpoints        = "private"
+#   member_host_flavor       = "multitenant"
+#   deletion_protection      = false
+#   service_credential_names = local.es_credentials
+# }
--- solutions/quickstart/outputs.tf
+++ solutions/quickstart/outputs.tf
@@ -51,107 +51,6 @@ output "watsonx_ai" {
 }
 
 ########################################################################################################################
-# watson Discovery
-########################################################################################################################
-
-output "watson_discovery" {
-  description = "Connection and configuration details for the Watson Discovery instance."
-  value = {
-    "crn"           = module.watson_discovery.crn
-    "account_id"    = module.watson_discovery.account_id
-    "guid"          = module.watson_discovery.guid
-    "name"          = module.watson_discovery.name
-    "plan_id"       = module.watson_discovery.plan_id
-    "dashboard_url" = module.watson_discovery.dashboard_url
-  }
-}
-
-########################################################################################################################
-# watsonx Assistant
-########################################################################################################################
-
-output "watsonx_assistant" {
-  description = "Connection and configuration details for the watsonx Assistant instance."
-  value = {
-    "crn"           = module.watsonx_assistant.crn
-    "account_id"    = module.watsonx_assistant.account_id
-    "guid"          = module.watsonx_assistant.guid
-    "name"          = module.watsonx_assistant.name
-    "plan_id"       = module.watsonx_assistant.plan_id
-    "dashboard_url" = module.watsonx_assistant.dashboard_url
-  }
-}
-
-########################################################################################################################
-# watsonx.governance
-########################################################################################################################
-
-output "watsonx_governance" {
-  description = "Connection and configuration details for the watsonx.governance instance."
-  value = {
-    "crn"           = module.watsonx_governance.crn
-    "account_id"    = module.watsonx_governance.account_id
-    "guid"          = module.watsonx_governance.guid
-    "name"          = module.watsonx_governance.name
-    "plan_id"       = module.watsonx_governance.plan_id
-    "dashboard_url" = module.watsonx_governance.dashboard_url
-  }
-}
-
-########################################################################################################################
-# watsonx.data
-########################################################################################################################
-
-output "watsonx_data" {
-  description = "Connection and configuration details for the watsonx.data instance."
-  value = {
-    "crn"           = module.watsonx_data.crn
-    "account_id"    = module.watsonx_data.account_id
-    "guid"          = module.watsonx_data.guid
-    "name"          = module.watsonx_data.name
-    "plan_id"       = module.watsonx_data.plan_id
-    "dashboard_url" = module.watsonx_data.dashboard_url
-  }
-}
-
-########################################################################################################################
-# watsonx.data
-########################################################################################################################
-
-output "watsonx_orchestrate" {
-  description = "Connection and configuration details for the watsonx Orchestrate instance."
-  value = {
-    "crn"           = module.watsonx_orchestrate.crn
-    "account_id"    = module.watsonx_orchestrate.account_id
-    "guid"          = module.watsonx_orchestrate.guid
-    "name"          = module.watsonx_orchestrate.name
-    "plan_id"       = module.watsonx_orchestrate.plan_id
-    "dashboard_url" = module.watsonx_orchestrate.dashboard_url
-  }
-}
-
-########################################################################################################################
-# ICD - ElasticSearch
-########################################################################################################################
-
-output "icd_elastic_search" {
-  description = "ICD Elastic search output."
-  value = {
-    "crn"      = module.icd_elasticsearch.crn
-    "id"       = module.icd_elasticsearch.id
-    "version"  = module.icd_elasticsearch.version
-    "hostname" = module.icd_elasticsearch.hostname
-    "port"     = module.icd_elasticsearch.port
-  }
-}
-
-output "icd_es_service_credentials_json" {
-  description = "ICD Elastic search credentials json."
-  value       = module.icd_elasticsearch.service_credentials_json
-  sensitive   = true
-}
-
-########################################################################################################################
 # Container Registry
 ########################################################################################################################
 output "icr_namespace_crn" {
@@ -171,3 +70,106 @@ output "code_engine" {
     "output_image" = local.output_image
   }
 }
+
+# For quick start variation, below resources are not required hence commented. Uncomment to use as per the requirement.
+
+# ########################################################################################################################
+# # watson Discovery
+# ########################################################################################################################
+
+# output "watson_discovery" {
+#   description = "Connection and configuration details for the Watson Discovery instance."
+#   value = {
+#     "crn"           = module.watson_discovery.crn
+#     "account_id"    = module.watson_discovery.account_id
+#     "guid"          = module.watson_discovery.guid
+#     "name"          = module.watson_discovery.name
+#     "plan_id"       = module.watson_discovery.plan_id
+#     "dashboard_url" = module.watson_discovery.dashboard_url
+#   }
+# }
+
+# ########################################################################################################################
+# # watsonx Assistant
+# ########################################################################################################################
+
+# output "watsonx_assistant" {
+#   description = "Connection and configuration details for the watsonx Assistant instance."
+#   value = {
+#     "crn"           = module.watsonx_assistant.crn
+#     "account_id"    = module.watsonx_assistant.account_id
+#     "guid"          = module.watsonx_assistant.guid
+#     "name"          = module.watsonx_assistant.name
+#     "plan_id"       = module.watsonx_assistant.plan_id
+#     "dashboard_url" = module.watsonx_assistant.dashboard_url
+#   }
+# }
+
+# ########################################################################################################################
+# # watsonx.governance
+# ########################################################################################################################
+
+# output "watsonx_governance" {
+#   description = "Connection and configuration details for the watsonx.governance instance."
+#   value = {
+#     "crn"           = module.watsonx_governance.crn
+#     "account_id"    = module.watsonx_governance.account_id
+#     "guid"          = module.watsonx_governance.guid
+#     "name"          = module.watsonx_governance.name
+#     "plan_id"       = module.watsonx_governance.plan_id
+#     "dashboard_url" = module.watsonx_governance.dashboard_url
+#   }
+# }
+
+# ########################################################################################################################
+# # watsonx.data
+# ########################################################################################################################
+
+# output "watsonx_data" {
+#   description = "Connection and configuration details for the watsonx.data instance."
+#   value = {
+#     "crn"           = module.watsonx_data.crn
+#     "account_id"    = module.watsonx_data.account_id
+#     "guid"          = module.watsonx_data.guid
+#     "name"          = module.watsonx_data.name
+#     "plan_id"       = module.watsonx_data.plan_id
+#     "dashboard_url" = module.watsonx_data.dashboard_url
+#   }
+# }
+
+# ########################################################################################################################
+# # watsonx.orchestrate
+# ########################################################################################################################
+
+# output "watsonx_orchestrate" {
+#   description = "Connection and configuration details for the watsonx Orchestrate instance."
+#   value = {
+#     "crn"           = module.watsonx_orchestrate.crn
+#     "account_id"    = module.watsonx_orchestrate.account_id
+#     "guid"          = module.watsonx_orchestrate.guid
+#     "name"          = module.watsonx_orchestrate.name
+#     "plan_id"       = module.watsonx_orchestrate.plan_id
+#     "dashboard_url" = module.watsonx_orchestrate.dashboard_url
+#   }
+# }
+
+# ########################################################################################################################
+# # ICD - ElasticSearch
+# ########################################################################################################################
+
+# output "icd_elastic_search" {
+#   description = "ICD Elastic search output."
+#   value = {
+#     "crn"      = module.icd_elasticsearch.crn
+#     "id"       = module.icd_elasticsearch.id
+#     "version"  = module.icd_elasticsearch.version
+#     "hostname" = module.icd_elasticsearch.hostname
+#     "port"     = module.icd_elasticsearch.port
+#   }
+# }
+
+# output "icd_es_service_credentials_json" {
+#   description = "ICD Elastic search credentials json."
+#   value       = module.icd_elasticsearch.service_credentials_json
+#   sensitive   = true
+# }
