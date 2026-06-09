a9b994ce75f6d55daf5da2ee07375d32ba8d98f0
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-06-09 03:46:13 +0000

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v10.16.5

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -36,7 +36,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "10.16.4"
+  version           = "10.16.5"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${local.prefix}cos"
   cos_plan          = "standard"
