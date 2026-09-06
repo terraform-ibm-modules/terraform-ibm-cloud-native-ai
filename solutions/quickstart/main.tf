6863b0c9f7b005244b491fca7832416d50939ef9
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-06 05:41:49 +0530

    fix(deps): update dependencies (#69)

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -36,7 +36,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "10.17.8"
+  version           = "10.17.11"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${local.prefix}cos"
   cos_plan          = "standard"
@@ -49,7 +49,7 @@ module "cos" {
 
 module "key_protect_all_inclusive" {
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.6.6"
+  version                     = "5.6.7"
   resource_group_id           = module.resource_group.resource_group_id
   region                      = var.region
   key_protect_instance_name   = "${local.prefix}kp"
